bd46b55d5b16962abed95dca2af1da747e5d60f2
alicaleonie05 <38717359+alicaleonie05@users.noreply.github.com>
2024-08-10 11:44:37 +0200

    change confidentials

--- experiment/jspsych-6.0.4/plugins/jspsych-link.js
+++ experiment/jspsych-6.0.4/plugins/jspsych-link.js
@@ -1,4 +1,4 @@
 var participant_id = new URLSearchParams(window.location.search).get('PROLIFIC_PID');
 function clickBack(){
-    window.location.href = "https://app.prolific.com/submissions/complete?cc=C1HKB6DK";
+    window.location.href = "https://github.com/alicaleonie05/UncertaintyBandit.git";
 }
